50fe82d46b25ac0b1e51188f8d14324d52304a79
UnfamiliarLegacy <74633542+UnfamiliarLegacy@users.noreply.github.com>
2025-04-17 06:23:12 +0200

    Update launch4j maven to 2.5.3

--- src/main/java/io/github/fvarrui/javapackager/maven/CreateWindowsExeLaunch4j.java
+++ src/main/java/io/github/fvarrui/javapackager/maven/CreateWindowsExeLaunch4j.java
@@ -115,7 +115,7 @@ public class CreateWindowsExeLaunch4j extends AbstractCreateWindowsExe {
 					plugin(
 							groupId("com.akathist.maven.plugins.launch4j"), 
 							artifactId("launch4j-maven-plugin"),
-							version("2.4.1")
+							version("2.5.3")
 					),
 					goal("launch4j"),
 					configuration(pluginConfig.toArray(new Element[0])),
